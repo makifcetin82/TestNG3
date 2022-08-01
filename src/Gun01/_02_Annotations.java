0759e2b711338da235665fcd56ba47067d6fa5c8
asus <makif_cetin@hotmail.com>
2022-08-01 23:24:47 +0300

    Initial commit

---
+++ src/Gun01/_02_Annotations.java
@@ -0,0 +1,46 @@
+package Gun01;
+
+import org.testng.annotations.*;
+
+public class _02_Annotations {
+
+/*
+
+      @BeforeClass--> Her class çalışıyorken önce burası çalışır.
+         @BeforeMetod--> Her metoddan önce çalışır.
+           @Test -->testlerin çalıştığı metodlar
+           @Test-->testlerin çalıştığı metodlar
+        @AfterMetod  --> Her metoddan sonra çalışır
+     @AfterClass--> Her class çalışıyorken sonra burası çalışır.
+*/
+
+
+    @BeforeClass
+     void beforeClass(){
+         System.out.println("her class dan önce çalışıcak :before class");  }
+
+     @BeforeMethod
+     void beforeMethod(){
+         System.out.println("her test methoddan önce çalışıcak:before method");
+     }
+      @Test
+     void test1(){
+         System.out.println("test 1 çalıştı");
+     }
+     @Test
+     void test2(){
+         System.out.println("test 2 çalıştı");
+     }
+
+
+     @AfterMethod
+     void afterMethod(){
+         System.out.println("her test methoddan sonra çalışıcak:after method");
+     }
+     @AfterClass
+     void afterClass(){
+         System.out.println("her class dan önce çalışıcak:After Class");
+     }
+
+
+}
---
+++ src/Gun01/_03_Assertions.java
@@ -0,0 +1,100 @@
+package Gun01;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _03_Assertions {
+     //Yeşil tik :testte hata yok
+    //Sarı çarpı testte hata var
+    //Beyaz : test çalıştırılmadı. skip
+
+      @Test
+      void  EqualOrnek(){
+
+          String s1="Merhaba";
+          String s2="İyi akşamlar";
+
+
+
+          //oluşan,beklenen,"hata başlığı"
+          Assert.assertEquals(s1,s2,"Karşılaştırma sonucu");
+          //actual:oluşan
+          //expected :beklenen
+
+      }
+
+
+
+
+    @Test
+    void  NotEqualOrnek(){
+
+        String s1="Merhaba";
+        String s2="İyi akşamlar";
+
+
+
+        //oluşan,beklenen,"hata başlığı"
+        Assert.assertNotEquals(s1,s2,"Karşılaştırma sonucu");
+        //actual:oluşan
+        //expected :beklenen
+
+    }
+
+
+
+
+    @Test
+    void  TrueOrnek(){
+
+       int s1=55;
+       int  s2=555;
+
+
+
+        //oluşan,beklenen,"hata başlığı"
+        Assert.assertTrue(s1==s2,"Karşılaştırma sonucu");
+        //actual:olan
+        //expected :beklenen
+
+    }
+
+
+
+
+    @Test
+    void  NullOrnek(){
+
+        String s1=null;
+
+
+
+
+        //oluşan,beklenen,"hata başlığı"
+        Assert.assertNull(s1,"Karşılaştırma sonucu");
+        //actual:olan
+        //expected :beklenen
+
+    }
+
+
+
+
+    @Test
+    void  direktFail(){
+
+        int a=55;
+
+        if(a==55)
+
+
+        //oluşan,beklenen,"hata başlığı"
+        Assert.fail();
+        //actual:oluşan
+        //expected :beklenen
+
+    }
+
+
+
+}
